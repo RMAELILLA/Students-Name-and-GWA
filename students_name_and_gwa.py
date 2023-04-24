e633047a5fd299995e66e355d071c1f3bc70f540
RMAELILLA <ronioryzamae12@gmail.com>
2023-04-24 21:50:50 +0800

    restart

--- students_name_and_gwa.py
+++ students_name_and_gwa.py
@@ -1,9 +1,4 @@
-import re
-students_gwa = []
 # read a file contains names of student together with gwa
-with open("students_name_gwa.txt", "r") as student_name_gwa, open("students_name.txt", "a") as student_name, open("students_gwa.txt", "a") as student_gwa:
+with open("students_name_gwa.txt", "r") as student_name_gwa:
     for line in student_name_gwa:
-        for i in re.findall(r'\d+(?:\.\d+)?', line):
-            students_gwa.append(i)
-            student_gwa.write(str(students_gwa) + "\n")
 # output name of student with the highest gwa
