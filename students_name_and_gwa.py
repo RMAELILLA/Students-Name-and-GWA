071421030aed5724f2a50fbf0a2d0ce8324297e6
RMAELILLA <ronioryzamae12@gmail.com>
2023-04-24 21:56:02 +0800

    add a process, to separate student name and gwa

--- students_name_and_gwa.py
+++ students_name_and_gwa.py
@@ -1,4 +1,8 @@
 # read a file contains names of student together with gwa
 with open("students_name_gwa.txt", "r") as student_name_gwa:
+    # split name and gwa
+    students_files = []
     for line in student_name_gwa:
+        name, gwa = line.strip().split(",")
+        students_files[name] = float(gwa)
 # output name of student with the highest gwa
