b2f50cf052f77553d689f776d3985376ab8d7dc7
RMAELILLA <ronioryzamae12@gmail.com>
2023-04-24 15:50:37 +0800

    read txt file

--- students_name_and_gwa.py
+++ students_name_and_gwa.py
@@ -1,2 +1,5 @@
 # read a file contains names of student together with gwa
+with open("student_name_gwa.txt", "r") as student_name_gwa:
+    for line in student_name_gwa:
+        
 # output name of student with the highest gwa
