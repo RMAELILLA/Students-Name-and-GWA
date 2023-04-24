f4426d3c1c78a42306844e2e1fe399d733c6acdd
RMAELILLA <ronioryzamae12@gmail.com>
2023-04-24 20:37:35 +0800

    error, argument should be a real number and not in list

--- students_name_and_gwa.py
+++ students_name_and_gwa.py
@@ -1,10 +1,13 @@
 import re
 student_gwa = []
+student_name = []
 # read a file contains names of student together with gwa
 with open("students_name_gwa.txt", "r") as student_name_gwa:
     for line in student_name_gwa:
         for i in re.findall(r'\d+(?:\.\d+)?', line):
             student_gwa.append(i)
-
+            if student_gwa >= 1.0:
+                for i in re.findall(r'\f'):
+                    student_name.append(i)
 print(student_gwa)
 # output name of student with the highest gwa
